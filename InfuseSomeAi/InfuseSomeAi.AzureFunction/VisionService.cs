3e73347b8134f973e87817d0ca852483d44474dc
Henk Boelman <henk@boelman.eu>
2017-06-21 13:16:14 +0200

    Empty Azure Function with Blob Trigger

---
+++ InfuseSomeAi/InfuseSomeAi.AzureFunction/VisionService.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace InfuseSomeAi.AzureFunction
+{
+    public static class VisionService
+    {
+        [FunctionName("ComputerVision")]        
+        public static void Run([BlobTrigger("images/{name}", Connection = "BlobStorageConnection")]Stream myBlob, string name, TraceWriter log)
+        {
+            log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
